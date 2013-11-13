665c5373fa903a138665b256befb94898843e549
Lane <ldesboro@hotmail.com>
2013-11-13 11:38:09 -0800

    with balloons and settimer updates

--- server.js
+++ server.js
@@ -27,11 +27,12 @@ var bgData = [];
 var cgmfile ;
 var treatmentfile ;
 var port = process.env.PORT || 1337;
-var refresh_rate = 5000; // initial scan, 5 seconds
+var ftp_refresh_rate = 121121; // ~121 seconds - a weird number so it doesn't coincide with refresh_rate and collide with file reads
 
 var simulated = false;
+var sim_start = new Date("2013-10-15T18:56:00.123Z").getTime();
 
-var now = (simulated === true ? new Date("2013-10-17T07:56:00.123Z").getTime() : new Date().getTime());
+var now = (simulated === true ? sim_start : new Date().getTime());
 
 var nodeStatic = require('node-static');
 var fs = require('fs');
@@ -78,18 +79,23 @@ var server = require('http').createServer(function serverCreator(request, respon
 //Setup socket io for data and message transmission
 var io = require('socket.io').listen(server); 
 
+function refresh_rate(){
+    var rr = (simulated? 3 : (initialize ? 15 : 60)) * 1000
+    return [rr];
+}
 
 function update() {
+    //console.log('updated:',new Date, '   displayed time:',new Date(now),' refresh rate:', new refresh_rate/1000)
+	
 	cgmfile = (simulated === true ? "hayden_oct.csv" : "hayden.csv");
 	treatmentfile = (simulated === true ? "treatment_oct.csv" : "treatment.csv");
 
     if (initialize === true) {
-        now = (simulated === true ? new Date("2013-10-17T07:56:00.123Z").getTime() : new Date().getTime());
+        now = (simulated === true ? sim_start : new Date().getTime());
     }
     else {
         now = (simulated === true ? now + 5 * 60 * 1000 : Date.now());
     }
-	refresh_rate = (simulated === true ? 0.02 : 1) * 60 * 1000 ;
 
     if (!simulated) {
         //	Grab csv from ftp site and store locally
@@ -105,10 +111,8 @@ function update() {
 		//	connect to FTP server to grab CSV and save it to Azure
 		setInterval(function() {
 			c.connect({host: ftp.host, user: ftp.user, password: ftp.password});
-		}, Math.round(refresh_rate*0.9));
+		}, Math.round(ftp_refresh_rate));
     }
-
-
 
 	//(re)loads the csv files, if necessary
 	if (!simulated || initialize) {
@@ -138,7 +142,7 @@ function update() {
 			treatment.unshift({ x: t, y: lines[i][2], insulin: lines[i][1], carbs: lines[i][3], CR: lines[i][4] });
 		}
 	}
-	io.sockets.emit("treatment", treatment);
+	//io.sockets.emit("treatment", treatment);
 
 	// parse the csv file into lines
 	var lines_with_duplicates = cgmdata.trim().split('\n');
@@ -158,68 +162,74 @@ function update() {
 		}
 	}
 
-	//	Predict using AR model
-	var predicted = [];
+	
 	var actual_len = actual.length - 1;
-	var lastValidReadingTime = actual[actual_len].x;
-	var elapsed_min = (actual[actual_len].x - actual[actual_len - 1].x) / (60 * 1000);
-	var BG_REF = 140;
-	var y = Math.log(actual[actual_len].y / BG_REF);
-	if (elapsed_min < 5.1) {
-		y = [Math.log(actual[actual_len - 1].y / BG_REF), y];
-	} else {
-		y = [y, y];
-	}
-	var n = Math.ceil(12 * (1 / 2 + (now - lastValidReadingTime) / 3600 / 1000));   //Predict 1/2 hour ahead
-	var AR = [-0.723, 1.716];                   //AR calculation constants
-	var dt = actual[actual_len].x;
-	for (i = 0; i <= n; i++) {
-		y = [y[1], AR[0] * y[0] + AR[1] * y[1]];
-		dt = dt + 5 * 60 * 1000;
-		predicted[i] = {
-				x: dt,
-				y: Math.max(36, Math.min(400, Math.round(BG_REF * Math.exp(y[1]))))
-		};
-	}
+    //is there data to send to client?
+	if (actual_len > 1) {
+        //	Predict using AR model
+	    var predicted = [];
+	    var lastValidReadingTime = actual[actual_len].x;
+	    var elapsed_min = (actual[actual_len].x - actual[actual_len - 1].x) / (60 * 1000);
+	    var BG_REF = 140;
+	    var y = Math.log(actual[actual_len].y / BG_REF);
+	    if (elapsed_min < 5.1) {
+	        y = [Math.log(actual[actual_len - 1].y / BG_REF), y];
+	    } else {
+	        y = [y, y];
+	    }
+	    var n = Math.ceil(12 * (1 / 2 + (now - lastValidReadingTime) / 3600 / 1000));   //Predict 1/2 hour ahead
+	    var AR = [-0.723, 1.716];                   //AR calculation constants
+	    var dt = actual[actual_len].x;
+	    for (i = 0; i <= n; i++) {
+	        y = [y[1], AR[0] * y[0] + AR[1] * y[1]];
+	        dt = dt + 5 * 60 * 1000;
+	        predicted[i] = {
+	            x: dt,
+	            y: Math.max(36, Math.min(400, Math.round(BG_REF * Math.exp(y[1]))))
+	        };
+	    }
 
-	//consolidate and send the data to the client
-	bgData = [actual, predicted, mbg, treatment];
-	io.sockets.emit("now", now)
-	io.sockets.emit("sgv", bgData);
+	    //consolidate and send the data to the client
+	    bgData = [actual, predicted, mbg, treatment];
+	    io.sockets.emit("now", now)
+	    io.sockets.emit("sgv", bgData);
 
-	//	compute current loss
-	var avgLoss = 0;
-	if (now > lastAckTime + 40 / 60 * 3600 * 1000) {
-		for (i = 0; i <= 6; i++) {
-			avgLoss += 1 / 6 * Math.pow(log10(predicted[i].y / 120), 2);
-		}
-		if (avgLoss > 0.10) {
-			io.sockets.emit('urgent_alarm');
-			appendlog('urgent_alarm');
-		} else if (avgLoss > 0.05) {
-			io.sockets.emit('alarm');
-			appendlog('alarm');
-		}
+	    //	compute current loss
+	    var avgLoss = 0;
+	    if (now > lastAckTime + 40 / 60 * 3600 * 1000) {
+	        for (i = 0; i <= 6; i++) {
+	            avgLoss += 1 / 6 * Math.pow(log10(predicted[i].y / 120), 2);
+	        }
+	        if (avgLoss > 0.10) {
+	            io.sockets.emit('urgent_alarm');
+	            //appendlog('urgent_alarm');
+	        } else if (avgLoss > 0.05) {
+	            io.sockets.emit('alarm');
+	            //appendlog('alarm');
+	        }
+	    }
 	}
 }
 
-var sensorReadID = setInterval(update, refresh_rate);
+var sensorReadID = setInterval(update, new refresh_rate);
 
 io.set('log level', 0); // reduce logging
 //Windows Azure Web Sites does not currently support WebSockets, so for long-polling
 io.configure(function () {                
 	io.set('transports', ['xhr-polling']);  
-});                                       
+});
 
 io.sockets.on('connection', function (socket) {
-	//socket.emit("treatment", treatment);
-	socket.emit("sgv", bgData);
-	socket.on('simulated', function(val) {
-		simulated = val; 
-		initialize = true; 
-		console.log('simulated = ',simulated) 
-	}) 
-	socket.on('ack', function(time) { lastAckTime = now; appendlog('ack') }) 
+    console.log("client connected, refresh rate",new refresh_rate)
+    //socket.emit("sgv", bgData);
+    socket.on('simulated', function (val) {
+        simulated = val;
+        initialize = true;
+        console.log('simulated = ', simulated)
+        clearInterval(sensorReadID);
+        sensorReadID = setInterval(update, new refresh_rate);
+    })
+    socket.on('ack', function (time) { lastAckTime = now; appendlog('ack') })
 });
 
 function log10(val) { return Math.log(val) / Math.LN10; }
